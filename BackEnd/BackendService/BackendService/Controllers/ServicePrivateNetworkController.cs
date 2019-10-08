1961888b89bc4b6914f9b5c1440bc654a8143b4d
menon <maneesh.menon@siemens-healthineers.com>
2019-10-08 13:21:42 +0530

    added backendservice with .net core 2.2

---
+++ BackEnd/BackendService/BackendService/Controllers/ServicePrivateNetworkController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BackendService.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Net;
+
+namespace BackendService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServicePrivateNetworkController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+
+            string str = await MakeWebRequest();
+
+            return str;
+        }
+
+
+        // POST: api/ServicePrivateNetwork
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ServicePrivateNetwork/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        private static async Task<string> MakeWebRequest()
+        {
+            //https://docs.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
+
+            string value = "";
+
+            var handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false,
+                AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip
+            };
+
+            handler.ServerCertificateCustomValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            try
+            {
+                using (var client = new HttpClient(handler))
+                {
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+                    client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+                    string str = BackendService.Startup.BackendServiceURLPrivateNetwork + BackendService.Startup.BackendRequestedServicePrivateNetwork;
+
+                    var stringTask = client.GetStringAsync(str);
+
+                    var msg = await stringTask;
+                    value = msg;
+                }
+            }
+            catch (Exception exception)
+            {
+                value = exception.Message;
+            }
+
+            return value;
+        }
+
+    }
+}
---
+++ BackEnd/BackendService/BackendService/Startup.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendService.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace BackendService
+{
+    public class Startup
+    {
+        public static string BackendServiceURLPublicInternet = "";
+        public static string BackendRequestedServicePublicInternet = "";
+
+        public static string BackendServiceURLPrivateNetwork = "";
+        public static string BackendRequestedServicePrivateNetwork = "";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public string ServiceURLPublicInternet
+        {
+            get
+            {
+                return this.Configuration["AppConfiguration:ServiceURLPublicInternet"];
+            }
+        }
+
+        public string RequestedServicePublicInternet
+        {
+            get
+            {
+                return this.Configuration["AppConfiguration:RequestedServicePublicInternet"];
+            }
+        }
+
+        public string ServiceURLPrivateNetwork
+        {
+            get
+            {
+                return this.Configuration["AppConfiguration:ServiceURLPrivateNetwork"];
+            }
+        }
+
+        public string RequestedServicePrivateNetwork
+        {
+            get
+            {
+                return this.Configuration["AppConfiguration:RequestedServicePrivateNetwork"];
+            }
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<AppConfiguration>(Configuration.GetSection("AppConfiguration"));
+            services.Configure<AppConfiguration>(Configuration);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            BackendServiceURLPublicInternet = ServiceURLPublicInternet;
+            BackendRequestedServicePublicInternet = RequestedServicePublicInternet;
+
+            BackendServiceURLPrivateNetwork = ServiceURLPrivateNetwork;
+            BackendRequestedServicePrivateNetwork = RequestedServicePrivateNetwork;
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
