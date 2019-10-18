61db60ec7bdc90c42e6a2e3b537f77393adbeab2
menon <maneesh.menon@siemens-healthineers.com>
2019-10-18 12:18:39 +0530

    renamed files

---
+++ Deployment/DeploymentScripts/script8_getdnsfqdn.sh
@@ -0,0 +1,23 @@
+#retrieve the fqdn dns name
+
+#load external file
+. ~/gitclouddrive/AKS/Deployment/DeploymentScripts/config.conf
+
+#get aks cluster location
+akslocation=$(az aks show  --resource-group $aksresourcegroup -n $aksname | grep location | cut -d':' -f 2 | cut -d ',' -f 1 | cut -d'"' -f 2)
+echo $akslocation
+
+publicipresourcegroupname="MC$delimiter$aksresourcegroup$delimiter$aksname$delimiter$akslocation"
+echo $publicipresourcegroupname
+
+value=$(az network public-ip show --resource-group $publicipresourcegroupname --name $publicipresourcename  | grep fqdn | cut -d':' -f 2 | cut -d ',' -f1 | cut -d'"' -f 2)
+
+var1='s/^(dnsname[[:blank:]]*:[[:blank:]]*).*/\1'
+var2=$value
+var3='/'
+var4="${var1}${var2}${var3}"
+sed -E $var4  ~/gitclouddrive/AKS/Deployment/HELM/ingresswithtls/values.yaml > result
+rm ~/gitclouddrive/AKS/Deployment/HELM/ingresswithtls/values.yaml
+mv result ~/gitclouddrive/AKS/Deployment/HELM/ingresswithtls/values.yaml
+
+dnsnameforingress=$value
