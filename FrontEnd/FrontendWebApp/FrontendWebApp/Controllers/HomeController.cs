48f3381fbdac6b03acf9c62febcd2a18f5aa8a7a
menon <maneesh.menon@siemens-healthineers.com>
2019-10-29 16:33:55 +0530

    update health for front end application

--- FrontEnd/FrontendWebApp/FrontendWebApp/Controllers/HomeController.cs
+++ FrontEnd/FrontendWebApp/FrontendWebApp/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace FrontendWebApp.Controllers
             return View();
         }
 
+        public IActionResult Healthz()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
