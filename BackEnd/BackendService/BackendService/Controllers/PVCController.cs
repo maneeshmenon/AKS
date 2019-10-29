43277dfdac205f9a988e85ff8c625ea30c8902f7
menon <maneesh.menon@siemens-healthineers.com>
2019-10-30 00:19:55 +0530

    added pvc

---
+++ BackEnd/BackendService/BackendService/Controllers/PVCController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PVCController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+
+            string str = GetFileInfo();
+
+            return str;
+        }
+
+        public string GetFileInfo()
+        {
+
+            DirectoryInfo dir = new DirectoryInfo("/mnt/azurediskdata");
+
+            var file = new FileInfo(Path.Combine(dir.FullName, "file1.txt"));
+            
+            if (!file.Exists)
+            {
+                using (Stream stream = file.OpenWrite())
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.WriteLine("This is a sample text message from a file named file1.txt created at a mounted path");
+                    writer.WriteLine("Path of the mounted volume is /mnt/azurediskdata");
+                    writer.WriteLine("");
+                    writer.WriteLine("");
+                    writer.WriteLine("To view the file's contents using Cloud Shell, execute the following commands:");
+                    writer.WriteLine("$kubectl exec -it <pod name> --/bin/bash");
+                    writer.WriteLine("$cd /mnt/azurediskdata");
+                    writer.WriteLine("$cat file1.yaml");
+                }
+            }
+            
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            foreach (System.IO.FileInfo fi in dir.GetFiles())
+            {
+                if(fi.Name.Equals("file1.txt"))
+                {
+                    using (System.IO.StreamReader reader = fi.OpenText())
+                    {
+                        sb.AppendLine(reader.ReadToEnd());
+                    }
+                }
+
+            }
+
+            return sb.ToString();
+        }
+    }
+}
