2fe84c956158df3f8a6e129e899fbaa693f9598a
menon <maneesh.menon@siemens-healthineers.com>
2019-10-29 14:29:08 +0530

    added health check to backend service

---
+++ BackEnd/BackendService/BackendService/Controllers/HealthController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        // GET: api/Health
+        [HttpGet]
+        public bool Get([FromHeader]string HealthProbe)
+        {
+            String headerToCompare = "KubernetesHealthProbe";
+
+            if ( (HealthProbe != null) && (HealthProbe != String.Empty))
+            {
+                if (HealthProbe.ToLower().Equals(headerToCompare.ToLower()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
