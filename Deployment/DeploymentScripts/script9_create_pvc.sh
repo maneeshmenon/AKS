57df562e15465da6062b64bffd196176fd505667
menon <maneesh.menon@siemens-healthineers.com>
2019-10-30 11:24:53 +0530

    updated scripts

---
+++ Deployment/DeploymentScripts/script9_create_pvc.sh
@@ -0,0 +1,5 @@
+#Deploy the following applications
+
+#Deploy pvc
+#kubectl apply -f ~/gitclouddrive/AKS/Deployment/KUBECTL/PersistentVolumeClaim.yaml
+helm install ~/gitclouddrive/AKS/Deployment/HELM/pvc
