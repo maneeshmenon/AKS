4f022dc1b5b45760dc3bb94be69e07cffb51769b
maneeshmenon <menon.maneesh@gmail.com>
2020-02-24 08:52:50 +0530

    Update script9_create_pvc.sh

--- Deployment/DeploymentScripts/script9_create_pvc.sh
+++ Deployment/DeploymentScripts/script9_create_pvc.sh
@@ -2,4 +2,4 @@
 
 #Deploy pvc
 #kubectl apply -f ~/gitclouddrive/AKS/Deployment/KUBECTL/PersistentVolumeClaim.yaml
-helm install ~/gitclouddrive/AKS/Deployment/HELM/pvc
+helm install pvc  ~/gitclouddrive/AKS/Deployment/HELM/pvc
